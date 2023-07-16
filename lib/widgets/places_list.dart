917fb43af45e345408a5626c564a79a12264a84c
Muhammad Usama <imusamairshad@gmail.com>
2023-07-16 22:36:38 +0500

    places_list.dart file is added.

---
+++ lib/widgets/places_list.dart
@@ -0,0 +1,44 @@
+import 'package:favorite_places/screens/places_detail.dart';
+import 'package:flutter/material.dart';
+import '../models/place.dart';
+
+class PlacesList extends StatelessWidget {
+  final List<Place> places;
+  const PlacesList({super.key, required this.places});
+
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return Center(
+        child: Text(
+          'No places added yet',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      );
+    }
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (ctx, index) {
+        return ListTile(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => PlacesDetailScreen(
+                  place: places[index],
+                ),
+              ),
+            );
+          },
+          title: Text(
+            places[index].title,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        );
+      },
+    );
+  }
+}
