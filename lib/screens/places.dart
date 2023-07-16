2b16bb28bacaa290c196b824c326b92d4479d491
Muhammad Usama <imusamairshad@gmail.com>
2023-07-16 22:37:30 +0500

    places.dart file is added.

---
+++ lib/screens/places.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../widgets/places_list.dart';
+import 'package:favorite_places/screens/add_place.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: const PlacesList(
+        places: [],
+      ),
+    );
+  }
+}
