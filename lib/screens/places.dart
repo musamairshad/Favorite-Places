de541a6a028df6ff688808965d7e289bd7ee830b
Muhammad Usama <imusamairshad@gmail.com>
2023-07-25 21:02:49 +0500

    places.dart is modified.

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../widgets/places_list.dart';
 import 'package:favorite_places/screens/add_place.dart';
+import 'package:favorite_places/providers/user_places.dart';
 
-class PlacesScreen extends StatelessWidget {
+// Before it was a StatelessWidget.
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
@@ -23,8 +27,9 @@ class PlacesScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const PlacesList(
-        places: [],
+      body: PlacesList(
+        // places: [],
+        places: userPlaces,
       ),
     );
   }
