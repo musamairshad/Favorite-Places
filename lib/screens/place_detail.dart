f96035e7c81223cf56ff9967a67d17ecf1bd6c1b
Muhammad Usama <imusamairshad@gmail.com>
2023-07-25 21:01:39 +0500

    place_detail.dart is modified.

---
+++ lib/screens/place_detail.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../models/place.dart';
+
+class PlaceDetailScreen extends StatelessWidget {
+  final Place place;
+  const PlaceDetailScreen({super.key, required this.place});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.title),
+      ),
+      body: Center(
+        child: Text(
+          place.title,
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
+  }
+}
