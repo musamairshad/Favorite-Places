07360b0a3e3f7678b16b1d236117f4330f62ff5a
Muhammad Usama <imusamairshad@gmail.com>
2023-08-10 23:49:01 +0500

    map.dart is modified.

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -22,6 +22,8 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,16 +39,26 @@ class _MapScreenState extends State<MapScreen> {
         ],
       ),
       body: GoogleMap(
+        onTap: (position) {
+          setState(() {
+            _pickedLocation = position;
+          });
+        },
         initialCameraPosition: CameraPosition(
             target: LatLng(widget.location.latitude, widget.location.longitude),
             zoom: 16),
-        markers: {
-          Marker(
-            markerId: const MarkerId('m1'),
-            position:
-                LatLng(widget.location.latitude, widget.location.longitude),
-          ),
-        },
+        markers: (_pickedLocation == null && widget.isSelecting)
+            ? {}
+            : {
+                Marker(
+                  markerId: const MarkerId('m1'),
+                  // position: _pickedLocation != null ? _pickedLocation! :
+                  //     LatLng(widget.location.latitude, widget.location.longitude),
+                  position: _pickedLocation ??
+                      LatLng(
+                          widget.location.latitude, widget.location.longitude),
+                ),
+              },
       ),
     );
   }
