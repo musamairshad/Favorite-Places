d1d25648f6ba00f105afa8a7a4aac8149593c9cd
Muhammad Usama <imusamairshad@gmail.com>
2023-08-13 12:45:10 +0500

    place.dart is modified.

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -18,6 +18,10 @@ class Place {
   final File image;
   final PlaceLocation location;
 
-  Place({required this.title, required this.image, required this.location})
-      : id = uuid.v4();
+  Place({
+    required this.title,
+    required this.image,
+    required this.location,
+    String? id,
+  }) : id = id ?? uuid.v4();
 }
